d6cf0ecde1c630142dd14bbb3f7e9e59761b20ea
sakariye <s235100@dtu.dk>
2025-10-23 13:30:24 +0200

    feat: remove package-lock.json and add Supabase client setup

---
+++ compliance/src/SupabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+    "https://ruohzjmsyoyuwdghusfq.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1b2h6am1zeW95dXdkZ2h1c2ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNTI3MjAsImV4cCI6MjA3NDYyODcyMH0.57g6XDQExWQXF0ZPG5LUlU8IQtX50USAJQ_tKncnGts"
+);
+export default supabase;
