015ee780241481ea2c66307af7eb325eaeb67481
miladibra1 <s215796@student.dtu.dk>
2025-11-27 06:51:49 +0100

    added email server and development scripts, updated dependencies, and configured proxy in Vite

--- compliance/vite.config.js
+++ compliance/vite.config.js
@@ -10,4 +10,12 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
